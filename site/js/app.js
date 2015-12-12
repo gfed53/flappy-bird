a7d0d51c10b33e91c4d3d371df611caf24f34890
gfed53 <gfederico7153@gmail.com>
2015-12-12 17:40:17 -0500

    Added defer properites to scripts, added 'experimental' action button for testing

--- site/js/app.js
+++ site/js/app.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("body").velocity("fadeIn", { duration: 1500 });
+	$("body").velocity("fadeIn", { duration: 1000 });
 	$(".lnk-home").on("click", function(e){
 		e.preventDefault();
 		console.log("Clicked btn");
@@ -8,7 +8,21 @@ $(function(){
 	$(".action a").on("click", function(e){
 		e.preventDefault();
 		console.log("clicked button");
-		$(this).velocity( { translateY: "-10px", backgroundColor: "#FFEB3B" }, 400 );
+		$(this).velocity( { "translateY": "-10px"}, 400 );
+	});
+
+	$(".action a").on("mouseenter", function(){
+		console.log("hover");
+		$(this).velocity({"border-radius": "0", "padding": "0.7em 0.7em"}, {"easing": "spring"});
+	}).on("mouseleave", function(){
+		console.log("leaving");
+		$(this).velocity("reverse");
+	});
+
+	$(".action input").on("click", function(){
+		console.log("this works?");
+		$(this).velocity({"translateX": "2em", "rotateZ": "360deg"}, {"easing": "easeIn"})
+		.velocity("reverse");
 	});
 });
 
