104b9375203352096729651243e7f445edd1b571
gfed53 <gfederico7153@gmail.com>
2015-12-13 10:38:57 -0500

    Removed old action button, changed animation

--- site/js/app.js
+++ site/js/app.js
@@ -5,23 +5,27 @@ $(function(){
 		console.log("Clicked btn");
 
 	});
-	$(".action a").on("click", function(e){
-		e.preventDefault();
-		console.log("clicked button");
-		$(this).velocity( { "translateY": "-10px"}, 400 );
-	});
+	// $(".action a").on("click", function(e){
+	// 	e.preventDefault();
+	// 	console.log("clicked button");
+	// 	$(this).velocity( { "translateY": "-10px"}, 400 );
+	// });
 
-	$(".action a").on("mouseenter", function(){
+	$(".action input").on("mouseenter", function(){
 		console.log("hover");
-		$(this).velocity({"border-radius": "0", "padding": "0.7em 0.7em"}, {"easing": "spring"});
+		$(this).velocity({"border-radius": "0", "padding": "0.7em"}, {"easing": "spring"});
 	}).on("mouseleave", function(){
 		console.log("leaving");
-		$(this).velocity("reverse");
+		$(this).velocity({"border-radius": "0.3em", "padding": "0.5em"}, {"easing": "spring"});
+		// .velocity({"translateX": "0", "translateY": "0", "rotateZ": "15deg"});
 	});
 
 	$(".action input").on("click", function(){
 		console.log("this works?");
-		$(this).velocity({"translateX": "2em", "rotateZ": "360deg"}, {"easing": "easeIn"})
+		$(this)
+		.velocity({"translateX": "0.5em", "translateY": "-0.5em", "rotateZ": "25deg"}, {"duration": 100, "easing": "easeIn"})
+		.velocity("reverse")
+		.velocity({"translateX": "-0.4em", "rotateZ": "-15deg"}, {"duration": 100})
 		.velocity("reverse");
 	});
 });
