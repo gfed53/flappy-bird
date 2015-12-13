b65fb54546a2dd821bc76e3ca1e0ee5f9b3bef88
gfed53 <gfederico7153@gmail.com>
2015-12-13 13:30:52 -0500

    removed experimental button

--- site/js/app.js
+++ site/js/app.js
@@ -4,24 +4,24 @@ $(function(){
 	$(".lnk-home").on("click", function(e){
 		e.preventDefault();
 		console.log("Clicked btn");
-
 	});
-	// $(".action a").on("click", function(e){
+	// $(".btn-action").on("click", function(e){
 	// 	e.preventDefault();
 	// 	console.log("clicked button");
 	// 	$(this).velocity( { "translateY": "-10px"}, 400 );
 	// });
 
-	$(".action input").on("mouseenter", function(){
+	$(".btn-action").on("mouseenter", function(){
 		console.log("hover");
-		$(this).velocity({"border-radius": "0", "padding": "0.7em"}, {"easing": "spring"});
-	}).on("mouseleave", function(){
+		$("a", this).velocity({"border-radius": "0", "padding": "0.7em"}, {"easing": "spring"});
+		}).on("mouseleave", function(){
 		console.log("leaving");
-		$(this).velocity({"border-radius": "0.3em", "padding": "0.5em"}, {"easing": "spring"});
+		$("a", this).velocity({"border-radius": "0.3em", "padding": "0.5em"}, {"easing": "spring"});
 		// .velocity({"translateX": "0", "translateY": "0", "rotateZ": "15deg"});
 	});
 
-	$(".action input").on("click", function(){
+	$(".btn-action").on("click", function(e){
+		e.preventDefault();
 		console.log("this works?");
 		$(this)
 		.velocity({"translateX": "0.5em", "translateY": "-0.5em", "rotateZ": "25deg"}, {"duration": 100, "easing": "easeIn"})
