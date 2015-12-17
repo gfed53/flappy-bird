6241f3adc1b8691d9cd98511eed88023029bb5f9
gfed53 <gfederico7153@gmail.com>
2015-12-17 11:12:27 -0500

    Added canvas, basic drawings for bird graphics

--- flappy-bird/js/app.js
+++ flappy-bird/js/app.js
@@ -1,10 +1,19 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var BirdGraphicsComponent = function(entity) {
 	this.entity = entity;
-};
+}
+step=0;
 
-BirdGraphicsComponent.prototype.draw = function(){
+BirdGraphicsComponent.prototype.draw = function(context){
+	context.beginPath();
+	context.arc(100+step, 100, 20, 0, 2 * Math.PI);
+	context.fill();
+	// context.fillStyle("#0000FF");
+	for(var i=0; i<10; i++){
+		context.fillRect(25+i, 25+i, 25, 25);
+	}
 	console.log("Drawing a bird.");
+	step+=1;
 };
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;
@@ -69,16 +78,34 @@ document.addEventListener('DOMContentLoaded', function() {
 },{"./flappy_bird":5}],7:[function(require,module,exports){
 var GraphicsSystem = function(entities) {
 	this.entities = entities;
+	// Canvas is where we draw
+	this.canvas = document.getElementById('main-canvas');
+	// Context is what we draw to
+	this.context = this.canvas.getContext('2d');
 };
 
 GraphicsSystem.prototype.run = function(){
 	// Tick the graphics system a few times to see it in action
-	for (var i=0; i<5; i++) {
-		this.tick();
-	}
+	// for (var i=0; i<5; i++) {
+	// 	this.tick();
+	// }
+
+	// Run the render loop
+	window.requestAnimationFrame(this.tick.bind(this));
 };
 
 GraphicsSystem.prototype.tick = function() {
+	// Set the canvas to the correct size if the window is resized
+	if (this.canvas.width != this.canvas.offsetWidth ||
+		this.canvas.height != this.canvas.offsetHeight) {
+		this.canvas.width = this.canvas.offsetWidth;
+		this.canvas.height = this.canvas.offsetHeight;
+	}
+	
+	// Clear the canvas
+	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+	//Rendering goes here
 	for(var i=0; i<this.entities.length; i++){
 		var entity = this.entities[i];
 		if (!"graphics" in entity.components){
@@ -87,6 +114,8 @@ GraphicsSystem.prototype.tick = function() {
 
 		entity.components.graphics.draw(this.context);
 	}
+	// Continue the render loop
+	window.requestAnimationFrame(this.tick.bind(this));	
 };
 
 exports.GraphicsSystem = GraphicsSystem;
--- flappy-bird/js/components/graphics/bird.js
+++ flappy-bird/js/components/graphics/bird.js
@@ -1,9 +1,18 @@
 var BirdGraphicsComponent = function(entity) {
 	this.entity = entity;
-};
+}
+step=0;
 
-BirdGraphicsComponent.prototype.draw = function(){
+BirdGraphicsComponent.prototype.draw = function(context){
+	context.beginPath();
+	context.arc(100+step, 100, 20, 0, 2 * Math.PI);
+	context.fill();
+	// context.fillStyle("#0000FF");
+	for(var i=0; i<10; i++){
+		context.fillRect(25+i, 25+i, 25, 25);
+	}
 	console.log("Drawing a bird.");
+	step+=1;
 };
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;
