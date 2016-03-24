85ed1b1efdc4493e335d91b2835dee4f5f284a11
gfed53 <gfederico7153@gmail.com>
2016-03-24 12:26:18 -0400

    Added styles/font to game overlay, cleaned up some comment clutter

--- flappy-bird/js/systems/input.js
+++ flappy-bird/js/systems/input.js
@@ -17,14 +17,6 @@ InputSystem.prototype.run = function(){
 	// On mobile, leaving both of these active creates a doubling-effect on touch. 
 	this.canvas.addEventListener('click', this.onClick.bind(this));
 	this.canvas.addEventListener('touchstart', this.onClick.bind(this), false);
-	// this.html.addEventListener('keydown', this.onKeyDown.bind(this), false);
-	//Why doesn't below work?
-	// this.html.addEventListener('keydown', function(e){
-	// 	if(e.keyCode === 32) {
-	// 		console.log(this);
-	// 		this.onSpace.bind(this);
-	// 	}
-	// });
 };
 
 InputSystem.prototype.onClick = function(e){
@@ -34,41 +26,5 @@ InputSystem.prototype.onClick = function(e){
 		bird.components.physics.velocity.y = 0.7;
 	}
 };
-
-// InputSystem.prototype.onKeyDown = function(){
-// 	if(this.paused === false){
-// 		console.log("pause");
-// 		//These are a new instance of "count". That's the problem?
-// 		this.graphics.count = 0;
-// 		this.physics.count = 0;
-// 		this.entities[0].components.physics.acceleration.y = 0;
-// 		this.entities[0].components.physics.velocity.y = 0;
-// 		console.log(this.graphics.count);
-// 		for(var i=3; i<this.entities.length; i++){
-// 			var entity = this.entities[i];
-// 			console.log(entity);
-// 			entity.components.physics.acceleration.y = 0;
-// 			entity.components.physics.velocity.y = 0;
-// 		}
-
-// 		this.paused = true;
-// 	} else {
-// 		this.paused = false;
-// 		console.log("unpaused");
-// 		for(var i=3; i<this.entities.length; i++){
-// 			var entity = this.entities[i];
-// 			console.log(entity);
-// 			entity.components.physics.acceleration.y = -0.01;
-// 			entity.components.physics.velocity.y = -0.02;
-// 		}
-// 		// this.entities[0].components.physics.acceleration.y = -2;
-// 	}
-// 	// e.preventDefault();
-// 	// window.clearInterval(this.physics.run.bind(this));
-	
-// 	console.log(this.entities[0]);
-// };
-
-
 
 exports.InputSystem = InputSystem;
--- flappy-bird/js/systems/physics.js
+++ flappy-bird/js/systems/physics.js
@@ -12,29 +12,9 @@ PhysicsSystem.prototype.run = function(){
 	window.setInterval(this.tick.bind(this), 1000 /60);
 	this.runControl();
 	//Runs our check for a locally stored high score on startup
-	// this.score.clearLocalHigh();
 	this.score.get();
 
 };
-
-//Counter(unused)
-// PhysicsSystem.prototype.counter = function(){
-// 	if(this.entities[0].components.status === "pause"){
-// 		this.count = 0;
-// 	} else{
-// 		this.count+=1;
-// 	}
-// 	console.log("physics: "+this.count);
-// }
-
-// PhysicsSystem.prototype.countDown = function(){
-// 	window.setInterval(this.counter.bind(this), 1000);
-// 	// console.log("physics ID "+window.setInterval(this.counter.bind(this), 1000));
-// }
-
-// PhysicsSystem.prototype.stopCount = function(){
-// 	window.clearInterval(4);
-// }
 
 //Upon collision, bird entity resets its position and count
 PhysicsSystem.prototype.reset = function(){
@@ -48,47 +28,32 @@ PhysicsSystem.prototype.reset = function(){
 
 //Control loop that bases velocity/acceleration on count
 PhysicsSystem.prototype.runControl = function(){
-	// console.log(this.entities[0].components.physics.acceleration.y);
 	window.setInterval(this.controlBird.bind(this), 1/60);
 	window.setInterval(this.controlPipes.bind(this), 1/60);
 };
 
 //Control loop for bird
 PhysicsSystem.prototype.controlBird = function(){
-	// console.log("running");
-	//To Change
 	if(this.entities[0].components.count>5){
 		this.entities[0].components.physics.acceleration.y = -2;
-		// console.log(this.entities[0].components.physics.acceleration.y);
 	} else {
 		this.entities[0].components.physics.acceleration.y = 0;
 		this.entities[0].components.physics.velocity.y = 0;
 	}
-	// else {
-	// 	this.entities[0].components.physics.acceleration.y = 0;
-	// }
-	//Still need to reset counter to 0!
 };
 
 //Control loop for pipes
-//In refactoring, maybe use a switch?
 PhysicsSystem.prototype.controlPipes = function(){
 	for(var i=3; i<this.entities.length; i++){
 		var entity = this.entities[i];
 		entityVeloc = this.entities[i].components.physics.velocity.x;
 		if(this.entities[0].components.count>35){
 			entity.components.physics.velocity.x = -0.8;
-		}
-		else if(this.entities[0].components.count>5){
-				// entity.components.physics.acceleration.x = -0.1;
+		} else if(this.entities[0].components.count>5){
 				entity.components.physics.velocity.x = -0.5;
-
 			} else if(this.entities[0].components.count<5) {
-				// entity.components.physics.acceleration.x = 0;
 				entity.components.physics.velocity.x = 0;
 			}
-			
-			// console.log(this.entities[i]);
 		}
 	};
 
@@ -101,16 +66,11 @@ PhysicsSystem.prototype.controlPipes = function(){
 			}
 
 			entity.components.physics.update(1/60);
-		// console.log(entity.components.physics.acceleration.x);
-		// console.log(entity.components.physics.velocity.x);
 	}
 
 	this.collisionSystem.tick();
 	this.score.update();
 	this.reset();
-	// console.log(this.entities[4].components.physics.acceleration.x);
-	// console.log(this.entities[4].components.physics.velocity.x);
-	
 };
 
 exports.PhysicsSystem = PhysicsSystem;
