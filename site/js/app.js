3b443e43c7f435ac9024da3d51d20dd5d550e436
gfed53 <gfederico7153@gmail.com>
2016-04-30 12:41:55 -0400

    Added second carousel, added breakpoint for Akuma image

--- site/js/app.js
+++ site/js/app.js
@@ -20,6 +20,7 @@
 // });
 jQuery(document).ready(function ($) {
 	
+	// Horizontal Carousel
 	var jssor_1_SlideshowTransitions = [
 	{$Duration:1200,$Opacity:2}
 	];
@@ -43,21 +44,50 @@ jQuery(document).ready(function ($) {
 	
             //responsive code begin
             //you can remove responsive code if you don't want the slider scales while window resizing
-            function ScaleSlider() {
-            	var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-            	if (refSize) {
-            		refSize = Math.min(refSize, 600);
-            		jssor_1_slider.$ScaleWidth(refSize);
-            	}
-            	else {
-            		window.setTimeout(ScaleSlider, 30);
-            	}
-            }
-            ScaleSlider();
-            $(window).bind("load", ScaleSlider);
-            $(window).bind("resize", ScaleSlider);
-            $(window).bind("orientationchange", ScaleSlider);
+            // function ScaleSlider() {
+            // 	var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+            // 	if (refSize) {
+            // 		refSize = Math.min(refSize, 600);
+            // 		jssor_1_slider.$ScaleWidth(refSize);
+            // 	}
+            // 	else {
+            // 		window.setTimeout(ScaleSlider, 30);
+            // 	}
+            // }
+            ScaleSlider(jssor_1_slider);
+            $(window).bind("load", ScaleSlider(jssor_1_slider));
+            $(window).bind("resize", ScaleSlider(jssor_1_slider));
+            $(window).bind("orientationchange", ScaleSlider(jssor_1_slider));
             //responsive code end
-        });
 
 
+// Vertical Carousel
+var jssor_2_options = {
+	$AutoPlay: true,
+	$DragOrientation: 2,
+	$PlayOrientation: 2,
+	$ArrowNavigatorOptions: {
+		$Class: $JssorArrowNavigator$
+	}
+};
+
+var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_2_options);
+
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            ScaleSlider(jssor_2_slider);
+            $(window).bind("load", ScaleSlider(jssor_2_slider));
+            $(window).bind("resize", ScaleSlider(jssor_2_slider));
+            $(window).bind("orientationchange", ScaleSlider(jssor_2_slider));
+            //responsive code end
+	function ScaleSlider(slider) {
+		var refSize = slider.$Elmt.parentNode.clientWidth;
+		if (refSize) {
+			refSize = Math.min(refSize, 600);
+			slider.$ScaleWidth(refSize);
+		}
+		else {
+			window.setTimeout(ScaleSlider, 30);
+		}
+	}
+});
