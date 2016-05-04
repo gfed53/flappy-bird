b1d9798be03048ba4bdafa8dbc608054c843d990
gfed53 <gfederico7153@gmail.com>
2016-05-04 15:35:11 -0400

    Added README, site: height/padding tweaks. game: collideRect inactive, to be removed

---
+++ README.md
@@ -0,0 +1,8 @@
+Flying Hadouken (Flappy Bird Clone) Landing Page and Game
+
+The Landing Page has a source directory (use 'site') and a build directory (use 'build').
+
+The Flappy Bird game's code is found under 'flappy-bird'. This is the source code.
+
+
+
--- flappy-bird/js/app.js
+++ flappy-bird/js/app.js
@@ -39,36 +39,36 @@ CircleCollisionComponent.prototype.collideCircle = function(entity){
 	return distanceSquared < radiusSum * radiusSum;
 };
 
-CircleCollisionComponent.prototype.collideRect = function(entity) {
-    var clamp = function(value, low, high) {
-        if (value < low) {
-            return low;
-        }
-        if (value > high) {
-            return high;
-        }
-        return value;
-    };
+// CircleCollisionComponent.prototype.collideRect = function(entity) {
+//     var clamp = function(value, low, high) {
+//         if (value < low) {
+//             return low;
+//         }
+//         if (value > high) {
+//             return high;
+//         }
+//         return value;
+//     };
 
-    var positionA = this.entity.components.physics.position;
-    var positionB = entity.components.physics.position;
-    var sizeB = entity.components.collision.size;
-    var closest = {
-        x: clamp(positionA.x, positionB.x - sizeB.x / 2,
-                 positionB.x + sizeB.x / 2),
-        y: clamp(positionA.y, positionB.y - sizeB.y / 2,
-                 positionB.y + sizeB.y / 2)
-    };
+//     var positionA = this.entity.components.physics.position;
+//     var positionB = entity.components.physics.position;
+//     var sizeB = entity.components.collision.size;
+//     var closest = {
+//         x: clamp(positionA.x, positionB.x - sizeB.x / 2,
+//                  positionB.x + sizeB.x / 2),
+//         y: clamp(positionA.y, positionB.y - sizeB.y / 2,
+//                  positionB.y + sizeB.y / 2)
+//     };
 
-    var radiusA = this.radius;
+//     var radiusA = this.radius;
 
-    var diff = {x: positionA.x - closest.x,
-                y: positionA.y - closest.y};
+//     var diff = {x: positionA.x - closest.x,
+//                 y: positionA.y - closest.y};
 
-    var distanceSquared = diff.x * diff.x + diff.y * diff.y;
+//     var distanceSquared = diff.x * diff.x + diff.y * diff.y;
 
-    return distanceSquared < radiusA * radiusA;
-};
+//     return distanceSquared < radiusA * radiusA;
+// };
 
 CircleCollisionComponent.prototype.collideRect2 = function(entity) {
     var clamp = function(value, low, high) {
